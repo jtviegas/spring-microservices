6e8c9355af8c8c9bfcf5504587479aaa3e5402d2
joao tiago viegas <joao.viegas@maersk.com>
2019-05-27 13:18:24 +0200

    finalized distributed config

--- solver-api/start.sh
+++ solver-api/start.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-this_folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-parent_folder=$(dirname $this_folder)
-
-whereis java
-ls
-
 debug_switch=""
 if [ ! -z "$DEBUG_PORT" ]; then
 	debug_switch="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=$DEBUG_PORT"
